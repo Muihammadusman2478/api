ec197db4fe216cd6f4d368154ed640e769265682
Muihammadusman2478 <57897349+Muihammadusman2478@users.noreply.github.com>
2025-03-26 06:34:35 +0500

    Create Bearer_token.sh

---
+++ Bearer_token.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Prompt the user for email and API key
+read -p "Enter your email: " email
+read -s -p "Enter your API key: " api_key
+echo ""
+
+# Fetch the access token
+access_token="$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" \
+    -X POST --data "{\"email\": \"$email\", \"api_key\": \"$api_key\"}" \
+    'https://api.cloudways.com/api/v1/oauth/access_token' | jq -r '.access_token')"
+
+# Print the token
+echo "Bearer Token: $access_token"
