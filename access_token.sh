3bb8de34ef4eeb87812be7ee2d43e0f63b8eeb15
Muihammadusman2478 <57897349+Muihammadusman2478@users.noreply.github.com>
2025-03-26 08:24:22 +0500

    Update access_token.sh

--- access_token.sh
+++ access_token.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Prompt the user for email and API key
+# Prompt for email and API key
 read -p "Enter your email: " email
-read -s -p "Enter your API key: " api_key
-echo ""
+read -sp "Enter your API key: " api_key
+echo  # Move to the next line after entering the API key
 
-# Fetch the access token
-export access_token="$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" \
-    -X POST --data "{\"email\": \"$email\", \"api_key\": \"$api_key\"}" \
-    'https://api.cloudways.com/api/v1/oauth/access_token' | jq -r '.access_token')"
+# Make API request to get the access token
+access_token=$(curl -s -X POST "https://api.cloudways.com/api/v1/oauth/access_token" \
+     -H "Accept: application/json" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     --data "email=$email&api_key=$api_key" | jq -r ".access_token")
 
-# Print the token
+# Print the token to CLI
 echo "Bearer Token: $access_token"
 
-# Reminder to use the token
+# Store the token in shell so it persists
+export access_token
 echo "To use the token in your shell, run: echo \$access_token"
